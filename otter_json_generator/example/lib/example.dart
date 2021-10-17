240b05c34c04b578592fb1cd4b900bddc9e5ff39
kam <hokamc00@gmail.com>
2021-10-17 22:14:04 +0800

    update example

--- otter_json_generator/example/lib/example.dart
+++ otter_json_generator/example/lib/example.dart
@@ -1,9 +1,14 @@
 library example;
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:otter_json/otter_json.dart';
 
-@ImportJsonModule([DefaultJsonModule])
+@ImportJsonModule([DefaultJsonModule, ExampleJsonModule])
 class ExampleImportJsonModule {}
+
+@JsonModule([ExampleSerializer])
+class ExampleJsonModule {}
 
 class Example implements Json {
   final String name;
@@ -13,6 +18,7 @@ class Example implements Json {
   final Map<String, int> map;
   final AnotherExample example2;
   final List<AnotherExample> example2s;
+  final Color color;
 
   Example({
     required this.name,
@@ -22,6 +28,7 @@ class Example implements Json {
     required this.map,
     required this.example2,
     required this.example2s,
+    required this.color,
   });
 }
 
@@ -29,4 +36,16 @@ class AnotherExample implements Json {
   final String name;
 
   AnotherExample({required this.name});
+}
+
+class ExampleSerializer implements JsonSerializer<Color, int> {
+  @override
+  Color decode(int output) {
+    return Color(output);
+  }
+
+  @override
+  int encode(Color input) {
+    return input.value;
+  }
 }
--- otter_json_generator/example/test/example_test.dart
+++ otter_json_generator/example/test/example_test.dart
@@ -1,5 +1,6 @@
 import 'package:example/example.dart';
 import 'package:example/json_module.gen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:otter_json/otter_json.dart';
 
@@ -16,15 +17,17 @@ void main() {
         map: {'abc': 10},
         example2: example2,
         example2s: [example2],
+        color: Colors.white,
       );
       expect(Otter.toJson(example),
-          '{"name":"name","age":1,"money":99.0,"list":[1,2],"map":{"abc":10},"example2":{"name":"name"},"example2s":[{"name":"name"}]}');
+          '{"name":"name","age":1,"money":99.0,"list":[1,2],"map":{"abc":10},"example2":{"name":"name"},"example2s":[{"name":"name"}],"color":4294967295}');
       expect(Otter.toMap(example), {
         'name': 'name',
         'age': 1,
         'money': 99.0,
         'list': [1, 2],
         'map': {'abc': 10},
+        'color': 4294967295,
         'example2': {'name': 'name'},
         'example2s': [
           {'name': 'name'}
